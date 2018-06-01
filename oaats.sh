e9e9a8916dec552012b125c82d814050fdbbc03c
fernfernfern <fernandomouco@gmail.com>
2018-06-01 22:54:08 +0100

    riiiight

--- oaats.sh
+++ oaats.sh
@@ -11,20 +11,20 @@ export OOATS_VCS_TYPE=$(echo $CIRCLE_BUILD_URL | grep / | cut -d/ -f4-6 | cut -d
 export OOATS_USERNAME=$(echo $CIRCLE_BUILD_URL | grep / | cut -d/ -f4-6 | cut -d/ -f2)
 export OOATS_PROJECT=$(echo $CIRCLE_BUILD_URL | grep / | cut -d/ -f4-6 | cut -d/ -f3)
 
-if [ $OOATS_VCS_TYPE -eq 'gh' ] ; then 
+if [ $OOATS_VCS_TYPE = 'gh' ] ; then 
     export OOATS_VCS_TYPE=github 
 fi
 
-if [ $OOATS_VCS_TYPE -eq 'bb' ] ; then 
+if [ $OOATS_VCS_TYPE = 'bb' ] ; then 
     export OOATS_VCS_TYPE=bitbucket 
 fi
 
-export $OOATS_VCS_TYPE
-export $OOATS_USERNAME
-export $OOATS_PROJECT
-export $NEXT_BUILD_DONE
+echo $OOATS_VCS_TYPE
+echo $OOATS_USERNAME
+echo $OOATS_PROJECT
+echo $NEXT_BUILD_DONE
 
-until [ $NEXT_BUILD_DONE -eq 1];
+until [ $NEXT_BUILD_DONE -eq 1 ];
 do
     BUILDS=$(curl -u ${OOATS_API_KEY}: https://circleci.com/api/v1.1/project/${OOATS_VCS_TYPE}/${OOATS_USERNAME}/${OOATS_PROJECT})
 
